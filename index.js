47ed46540d616df1c69cf81ee29643d7d11389b3
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-14 16:38:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var test = string;
-  if(test.toLowerCase() === test) {
+
+  if(string.toLowerCase() === string) {
     return "I can't hear you!"
-  } else if(string.toUpperCase() === test) {
+  } else if(string.toUpperCase() === string) {
     return "YES INDEED!"
-  } else if(test === "I love you, Grandma.") {
+  } else if(string === "I love you, Grandma.") {
     return "I love you, too."
   }
 
